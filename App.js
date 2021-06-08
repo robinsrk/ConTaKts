e7da589b6b2d718314074a9defe39425e5640fba
robinsrk <robinsrk3@gmail.com>
2021-06-09 04:47:17 +0600

    trying to fix bugs

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  Appearance,
-  Button,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-} from "react-native";
+import { Text, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <Text>Hello world</Text>
+      <SafeAreaView>
+        <Text>I am robin</Text>
       </SafeAreaView>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-});
---
+++ src/navigations/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native";
+const AppNavContainer = () => {
+  return <Text>Hello world</Text>;
+};
+
+export default AppNavContainer;
